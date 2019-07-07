f2eda57cf19a949b6d2077620c4adb70995a8c1e
Illia <nikondotlg@gmail.com>
2019-07-07 13:21:23 +0300

    initial commit

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const a1 = +prompt('Enter X value for A.');
+const a2 = +prompt('Enter Y value for A.');
+const b1 = +prompt('Enter X value for B.');
+const b2 = +prompt('Enter Y value for B.');
+const c1 = +prompt('Enter X value for C.');
+const c2 = +prompt('Enter Y value for C.');
+const half = 2;
+if ((a1 + b1) / half === c1 && (a2 + b2) / half === c2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// receiving triangle sides information.
+let a = +prompt('Please, enter A value of your triangle!');
+let b = +prompt('Please, enter B value of your triangle!');
+let c = +prompt('Please, enter C value of your triangle!');
+// check if entered num is positive and > 0, if triangle exists, its type.
+if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+  console.log('Triangle doesn’t exist');
+} else if (a === b && b === c) {
+  console.log('Equivalent triangle');
+} else if (a === b || b === c || a === c) {
+  console.log('Isosceles triangle');
+} else {
+  console.log('Normal triangle');
+}
