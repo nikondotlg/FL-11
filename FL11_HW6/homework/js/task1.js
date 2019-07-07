d84fdeac9ace7ade5f67b8b81033153add0cfd6c
Illia <nikondotlg@gmail.com>
2019-07-07 17:38:38 +0300

    fixes variables and adds comments

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,4 +1,4 @@
-// Your code goes here
+// receiving information
 const a1 = +prompt('Enter X value for A.');
 const a2 = +prompt('Enter Y value for A.');
 const b1 = +prompt('Enter X value for B.');
@@ -6,6 +6,7 @@ const b2 = +prompt('Enter Y value for B.');
 const c1 = +prompt('Enter X value for C.');
 const c2 = +prompt('Enter Y value for C.');
 const half = 2;
+// check if C divides by half
 if ((a1 + b1) / half === c1 && (a2 + b2) / half === c2) {
   console.log(true);
 } else {
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,8 +1,8 @@
 // receiving triangle sides information.
-let a = +prompt('Please, enter A value of your triangle!');
-let b = +prompt('Please, enter B value of your triangle!');
-let c = +prompt('Please, enter C value of your triangle!');
-// check if entered num is positive and > 0, if triangle exists, its type.
+const a = +prompt('Please, enter A value of your triangle!');
+const b = +prompt('Please, enter B value of your triangle!');
+const c = +prompt('Please, enter C value of your triangle!');
+// check if entered num is positive and > 0; if triangle exists; its type.
 if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
   console.log('Triangle doesn’t exist');
 } else if (a === b && b === c) {
