b2f1cc5d32869bebe7277e0b8f8219a8224063aa
Illia <nikondotlg@gmail.com>
2019-07-09 23:24:29 +0300

    removes logging and beautify file

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -7,44 +7,42 @@ const half = 2;
 const maxNumAdd = 4;
 
 if (confirm('Do you want to play a game?')) {
-    let totalAmount = 0;
-    const minNum = 0;
-    let maxNum = 8;
-    let chosenNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-    console.log('chosenNumber', chosenNumber);
-    let multiplier = 1;
-    for (let i = 1; i <= maxAttempts; i++) {
-        const promptMessage = `Choose a roulette pocket number from ${minNum} to ${maxNum}. 
+  let totalAmount = 0;
+  const minNum = 0;
+  let maxNum = 8;
+  let chosenNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+  let multiplier = 1;
+  for (let i = 1; i <= maxAttempts; i++) {
+    const promptMessage = `Choose a roulette pocket number from ${minNum} to ${maxNum}. 
             Attempts left: ${maxAttempts + 1 - i}
             Total prize: ${totalAmount}
             Possible prize on current attempt: ${wins[i - 1] * multiplier}$`;
-        if (+prompt(promptMessage) === chosenNumber) {
-            totalAmount += wins[i - 1] * multiplier;
-            console.log('totalAmount', totalAmount);
-            if (confirm(`Congratulation, you won! Your prize is: ${totalAmount} $. Do you want to continue?`)) {
-                i = 0;
-                multiplier = multiplier * half;
-                maxNum += maxNumAdd;
-                chosenNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-            } else {
-                alert(`Thank you for your participation. Your prize is ${totalAmount} $.`);
-                if (confirm(`Do you want to play again?`)) {
-                    i = 0;
-                    multiplier = multiplier * half;
-                    maxNum += maxNumAdd;
-                    chosenNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-                } else {
-                    break;
-                }
-            }
+    if (+prompt(promptMessage) === chosenNumber) {
+      totalAmount += wins[i - 1] * multiplier;
+      if (confirm(`Congratulation, you won! Your prize is: ${totalAmount} $. Do you want to continue?`)) {
+        i = 0;
+        multiplier = multiplier * half;
+        maxNum += maxNumAdd;
+        chosenNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+      } else {
+        alert(`Thank you for your participation. Your prize is ${totalAmount} $.`);
+        if (confirm(`Do you want to play again?`)) {
+          i = 0;
+          multiplier = multiplier * half;
+          maxNum += maxNumAdd;
+          chosenNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        } else {
+          break;
         }
-        if (i === maxAttempts) {
-            alert(`Thank you for your participation. Your prize is ${totalAmount} $.`);
-            if (confirm(`Do you want to play again?`)) {
-                i = 0;
-            }
-        }
+      }
     }
+    if (i === maxAttempts) {
+      alert(`Thank you for your participation. Your prize is ${totalAmount} $.`);
+      if (confirm(`Do you want to play again?`)) {
+        i = 0;
+      }
+    }
+  }
 } else {
-    alert('You did not become a billionaire, but can.');
+  alert('You did not become a billionaire, but can.');
 }
